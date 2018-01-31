8b33cd621d5acbaf2e5fe30c6a03c6fb8f5e7a76
raomukesh <er.mukeshyadav@gmail.com>
2018-01-31 16:57:18 +0530

    Add Controller & Service

--- app/mainController.js
+++ app/mainController.js
@@ -1,7 +1,14 @@
 'use strict';
 
-angular.module('myApp').controller('mainController');
+angular.module('myApp')
+.controller('mainController', function mainController($scope, mainService){
 
-function mainController ($scope){
-    
-};
+    $scope.nameFilter = null;
+    $scope.driversList = [];
+
+    mainService.getDriverDetails().success(function (response) {
+        //Dig into the responde to get the relevant data
+        $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+
+});
